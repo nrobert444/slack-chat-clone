d9e430f106969bd0a2690950bf518f8e43ac9278
Nicholas Roberto <njr444@gmail.com>
2020-09-23 09:31:01 -0700

    completed project with console logs removed.Styling could be improved and authentication/database for users etc. could also be added to build out.

--- slack/public/joinNs.js
+++ slack/public/joinNs.js
@@ -1,15 +1,12 @@
 function joinNs(endpoint) {
   if (nsSocket) {
-    // check to see if nsSocket is actually a socket
     nsSocket.close()
-    // remove the eventListener before it's added again
     document
       .querySelector('#user-input')
       .removeEventListener('submit', formSubmission)
   }
   nsSocket = io(`http://localhost:9000${endpoint}`)
   nsSocket.on('nsRoomLoad', nsRooms => {
-    // console.log(nsRooms)
     let roomList = document.querySelector('.room-list')
     roomList.innerHTML = ''
     nsRooms.forEach(room => {
@@ -21,21 +18,17 @@ function joinNs(endpoint) {
       }
       roomList.innerHTML += `<li class="room"><span class="glyphicon glyphicon-${glyph}"></span>${room.roomTitle}</li>`
     })
-    // add click listener to each room
     let roomNodes = document.getElementsByClassName('room')
     Array.from(roomNodes).forEach(elem => {
       elem.addEventListener('click', e => {
-        // console.log("Somone clicked on ",e.target.innerText);
         joinRoom(e.target.innerText)
       })
     })
-    // add room automatically... first time here
     const topRoom = document.querySelector('.room')
     const topRoomName = topRoom.innerText
     joinRoom(topRoomName)
   })
   nsSocket.on('messageToClients', msg => {
-    console.log(msg)
     const newMsg = buildHTML(msg)
     document.querySelector('#messages').innerHTML += newMsg
   })
--- slack/public/joinRoom.js
+++ slack/public/joinRoom.js
@@ -1,13 +1,10 @@
 function joinRoom(roomName) {
-  // Send this roomName to the server!
   nsSocket.emit('joinRoom', roomName, newNumberOfMembers => {
-    // we want to update the room member total now that we have joined!
     document.querySelector(
       '.curr-room-num-users'
     ).innerHTML = `${newNumberOfMembers} <span class="glyphicon glyphicon-user"></span>`
   })
   nsSocket.on('historyCatchUp', history => {
-    // console.log(history)
     const messagesUl = document.querySelector('#messages')
     messagesUl.innerHTML = ''
     history.forEach(msg => {
@@ -31,7 +28,6 @@ function joinRoom(roomName) {
       if (
         msg.innerText.toLowerCase().indexOf(e.target.value.toLowerCase()) === -1
       ) {
-        // the msg does not contain the user search term!
         msg.style.display = 'none'
       } else {
         msg.style.display = 'block'
